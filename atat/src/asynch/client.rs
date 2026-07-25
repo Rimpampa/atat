4d4cd3db44e68f1ff21a0dc1ec3e7aae655eeff7
Riccardo Ripanti <riccardo.ripanti01@gmail.com>
2026-07-25 18:08:45 +0200

    Reset the response slot when requesting the inner writer

--- atat/src/asynch/client.rs
+++ atat/src/asynch/client.rs
@@ -38,6 +38,7 @@ impl<'a, W: Write, const INGRESS_BUF_SIZE: usize> Client<'a, W, INGRESS_BUF_SIZE
 
     /// Returns a mutable reference to the inner writer.
     pub async fn inner(&mut self) -> &mut W {
+        self.res_slot.reset();
         self.wait_cooldown_timer().await;
         &mut self.writer
     }
